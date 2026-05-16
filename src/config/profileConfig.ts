cca5d54ec53fbc6ab45c9eedf57942ceb01094c5
blueshirt233 <2644536256@qq.com>
2026-05-16 18:04:10 +0800

    again

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/42.png",
+	avatar: "https://tu.2644536256.date/file/1778925756834_52.gif",
 
 	// 名字
 	name: "好想喝点奶绿",
