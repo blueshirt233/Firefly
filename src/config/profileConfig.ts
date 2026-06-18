20b46bd86ce582a4c44fb6dce03c5053dde0a30d
unknown <chenlj@shipgroup.net>
2026-06-18 21:51:38 +0800

    again

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,55 @@
+import type { ProfileConfig } from "../types/profileConfig";
+
+export const profileConfig: ProfileConfig = {
+	// 头像
+	// 图片路径支持三种格式：
+	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
+	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
+	// 3. 远程 URL："https://example.com/avatar.jpg"
+	avatar: "https://tu.2644536256.date/file/奶绿/1778925756834_52.gif",
+
+	// 名字
+	name: "好想喝点奶绿",
+
+	// 个人签名
+	bio: "风里的星星也很亮",
+
+	// 链接配置
+	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
+	// 访问https://icones.js.org/ 获取图标代码，
+	// 如果想使用尚未包含相应的图标集，则需要安装它
+	// `pnpm add @iconify-json/<icon-set-name>`
+	// showName: true 时显示图标和名称，false 时只显示图标
+	links: [
+/*		{
+			name: "GitHub",
+			icon: "fa7-brands:github",
+			url: "https://github.com/",
+			showName: false,
+		},
+		{
+			name: "Email",
+			icon: "fa7-solid:envelope",
+			url: "mailto:seasir666@gmail.com",
+			showName: false,
+		},
+		{
+			name: "RSS",
+			icon: "fa7-solid:rss",
+			url: "/rss/",
+			showName: false,
+		},
+		{
+			name: "cnb",
+			icon: "tdesign:logo-cnb-filled",
+			url: "https://cnb.cool/W3C/Hyde/Firefly-hyde",
+			showName: false,
+		},
+		{
+			name: "Telegram",
+			icon: "fa7-brands:telegram",
+			url: "https://t.me/seasir_Bot",
+			showName: false,
+		},*/
+	],
+};
